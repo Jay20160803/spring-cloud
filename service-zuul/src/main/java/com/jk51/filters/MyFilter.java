72a90a16843fb85435799d38f7e22e93e7285a8a
gaojie <472781184@qq.com>
2018-01-23 15:29:03 +0800

    11

---
+++ service-zuul/src/main/java/com/jk51/filters/MyFilter.java
@@ -0,0 +1,63 @@
+package com.jk51.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ * 版权所有(C) 2017 上海银路投资管理有限公司
+ * 描述:
+ * 作者: gaojie
+ * 创建日期: 2018-01-23
+ * 修改记录:
+ */
+@Component
+public class MyFilter extends ZuulFilter {
+
+    private Logger logger = LoggerFactory.getLogger(MyFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+
+        logger.info("{}>>>>>>>>>>>{}",request.getMethod(),request.getRequestURL().toString());
+
+        Object accessToken = request.getParameter("token");
+
+        if(accessToken==null){
+            logger.warn("token is empty");
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(401);
+            try {
+                context.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){}
+
+            return null;
+        }
+
+        logger.info("ok");
+        return null;
+    }
+}
